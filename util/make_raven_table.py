224a5fac58afc3e6444b529f85b4aa99dc4716ee
Benjamin Henne <13474968+benjaminhenne@users.noreply.github.com>
2024-03-09 16:09:31 +0100

    Added arguments for file directory, output path and config location and docstrings

--- util/make_raven_table.py
+++ util/make_raven_table.py
@@ -1,12 +1,20 @@
 import argparse
-import logging
-from pathlib import Path
 import json
-import yaml
+import logging
 from os.path import join
+from pathlib import Path
+
+import yaml
 
 
 def make_raven_tables(file_path: str, config_path: str, output_path: str = ""):
+    """Converts WhisperSeg inference results into Raven selection tables
+
+    Args:
+        file_path (str): Path to .jsonr inference files.
+        config_path (str): Path to the config file detailing all annotation classes.
+        output_path (str, optional): Path to an output directory. Defaults to the input directory.
+    """
     with open(config_path, 'r') as f:
         classes = yaml.safe_load(f)
         classes.pop('misc', None) # to remove labels such as 'fluff' and 'help'
@@ -21,8 +29,12 @@ def make_raven_tables(file_path: str, config_path: str, output_path: str = ""):
                 out.append(f"{id}\tWaveform 1\t1\t{res_file['onset'][i]:.3f}\t{res_file['offset'][i]:.3f}\t0.000\t8000.000\t{res_file['offset'][i]-res_file['onset'][i]:.4f}\t8000.000\t\t{res_file['cluster'][i]}")
                 out.append(f"{id}\tSpectrogram 1\t1\t{res_file['onset'][i]:.3f}\t{res_file['offset'][i]:.3f}\t0.000\t8000.000\t{res_file['offset'][i]-res_file['onset'][i]:.4f}\t8000.000\t\t{res_file['cluster'][i]}")
                 id += 1
-        out_path = join(p.absolute().parent, p.stem + "_PRED.Table.1.selections.txt")
         out_str = '\n'.join(o for o in out)
+        if output_path == "":
+            new_path = p.parent.absolute()
+        else:
+            new_path = output_path
+        out_path = join(new_path, p.stem + "_PRED.Table.1.selections.txt")
         with open(out_path, "w") as f:
             f.writelines(out_str)
 
@@ -30,4 +42,11 @@ def make_raven_tables(file_path: str, config_path: str, output_path: str = ""):
 if __name__ == "__main__":
     logging.basicConfig()
     logging.getLogger().setLevel(logging.INFO)
-    make_raven_tables("data/", "config/classes.yaml")
+
+    parser = argparse.ArgumentParser(description="Converts WhisperSeg inference results into Raven selection tables")
+    parser.add_argument("-p", "--file_path", type=str, help="Path to .jsonr inference files", required=True)
+    parser.add_argument("-c", "--config_path", type=str, help="Path to the config file detailing all annotation classes", default='./config/classes.yaml')
+    parser.add_argument("-o", "--output_path", type=str, help="Path to the output .txt  selection table files. Defaults to the input directory.", default="")
+    args = parser.parse_args()
+
+    make_raven_tables(**vars(args))
