d98890d8fb5c95246898e96d2f0ebfbcc9367954
Benjamin Henne <13474968+benjaminhenne@users.noreply.github.com>
2024-03-24 17:14:30 +0100

    Made the script callable

--- convert_hf_to_ct2.py
+++ convert_hf_to_ct2.py
@@ -1,38 +1,41 @@
-import os
-from transformers import WhisperTokenizer, WhisperConfig
 import argparse
+
 import ctranslate2
-import numpy as np
+from transformers import WhisperConfig, WhisperTokenizer
+
+
+def convert_hf_to_ct2(
+        model: str, output_dir: str, quantization: str = None, load_as_float16: bool = False,
+        low_cpu_mem_usage: bool = False, trust_remote_code: bool = False, force: bool = False,
+    ) -> None:
+    converter = ctranslate2.converters.TransformersConverter(
+        model_name_or_path = model,
+        load_as_float16 = load_as_float16,
+        low_cpu_mem_usage = low_cpu_mem_usage,
+        trust_remote_code = trust_remote_code,
+    )
+    converter.convert(
+        output_dir = output_dir,
+        quantization = quantization,
+        force = force,
+    )
+    ## save original tokenizer
+    tokenizer = WhisperTokenizer.from_pretrained(model)
+    tokenizer.save_pretrained(output_dir + "/hf_model")
+    ## save original model configuration
+    config = WhisperConfig.from_pretrained(model)
+    config.save_pretrained(output_dir + "/hf_model")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model", type=str,  )
-    parser.add_argument("--output_dir", type=str,  )
-    parser.add_argument("--quantization", default = None, help="can be int8, float16 and int8_float16, if it is not set, then do not apply quantization"  )
-    parser.add_argument("--load_as_float16", action="store_true" )
-    parser.add_argument("--low_cpu_mem_usage", action="store_true" )
-    parser.add_argument("--trust_remote_code", action="store_true" )
-    parser.add_argument("--force", action="store_true" )
-    
+    parser.add_argument("--model", type=str, required=True)
+    parser.add_argument("--output_dir", type=str, required=True)
+    parser.add_argument("--quantization", default = None, help="can be int8, float16 and int8_float16, if it is not set, then do not apply quantization")
+    parser.add_argument("--load_as_float16", action="store_true")
+    parser.add_argument("--low_cpu_mem_usage", action="store_true")
+    parser.add_argument("--trust_remote_code", action="store_true")
+    parser.add_argument("--force", action="store_true")
     args = parser.parse_args()
-        
-    converter = ctranslate2.converters.TransformersConverter(
-        model_name_or_path = args.model,
-        load_as_float16 = args.load_as_float16,
-        low_cpu_mem_usage = args.low_cpu_mem_usage,
-        trust_remote_code = args.trust_remote_code
-    )
-    
-    converter.convert(
-        output_dir = args.output_dir, 
-        quantization = args.quantization, 
-        force = args.force
-    )
-    
-    ## save original tokenizer
-    tokenizer = WhisperTokenizer.from_pretrained( args.model )
-    tokenizer.save_pretrained( args.output_dir + "/hf_model" )
-    
-    ## save original model configuration
-    config = WhisperConfig.from_pretrained( args.model )
-    config.save_pretrained( args.output_dir + "/hf_model" )
+
+    convert_hf_to_ct2(**vars(args))
