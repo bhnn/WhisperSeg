b09c50eea6fc2c8060fa6fca184224eff17adea7
Benjamin Henne <13474968+benjaminhenne@users.noreply.github.com>
2024-03-24 17:18:51 +0100

    Improved infer script

--- jobs/wseg_infer.sh
+++ jobs/wseg_infer.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
 
-#SBATCH --gres=gpu:rtx5000:4
+# SLURM directives
+#SBATCH --gres=gpu:rtx5000:1
+#SBATCH --mem 128G
+#SBATCH -c 32
 #SBATCH -p gpu
 #SBATCH -t 2-00:00:00
 #SBATCH -o /usr/users/bhenne/projects/whisperseg/slurm_files/job-%J.out
 
+# Define paths
+base="/usr/users/bhenne/projects/whisperseg"
+code="$base"
+script="infer.py"
+data="$base/data/lemur_snippet_1/train"
+mtype="final_checkpoint_ct2" # final_checkpoint or final_checkpoint_ct2 for faster inference
+model="$base/model/whisperseg-lemur-finetuned-snip1/$mtype"
+output="$base/results"
+work="/local/eckerlab/wseg_data"
+
+# Prepare modules and conda environment for compute node
 module load anaconda3
 source activate wseg
 
-cd /local/eckerlab/
+# Create temporary job directory and copy data
+destination="$work/$(date +"%Y%m%d_%H%M%S")_${script%.*}"
+mkdir -p "$destination"
+cp -r "$data"/* "$destination"
 
-if [ ! -d "wseg_data" ]
-then
-    mkdir wseg_data
-    cp -r /usr/users/bhenne/projects/whisperseg/data/lemur_snippet_1/train/ wseg_data
+# Run script || true, ensuring cleanup even on failure
+python "$code/$script" --data_dir "$destination" --model_path "$model" --output_dir "$output" || true
+
+# Clean up: remove data, "<time>_<job>/" directory and parent directory, if empty
+rm -rf "$destination"
+if [ -z "$(ls -A "${destination%/*}")" ]; then
+    rmdir "${destination%/*}"
 fi
-
-source activate wseg
-
-# -d data/lemur_snippet_1/train/
-dataset_path="/local/eckerlab/wseg_data"
-model_path="/usr/users/bhenne/projects/whisperseg/model/whisperseg-lemur-finetuned-snip1/final_checkpoint_ct2"
-output_dir="/usr/users/bhenne/projects/whisperseg/results"
-
-python infer.py --data_dir "$dataset_path" --model_path "$model_path" --output_dir "$output_dir" 
-
-rm -rf /local/eckerlab/wseg_data
