848769bb458319bff01ba5325bffef04ff829373
Benjamin Henne <13474968+benjaminhenne@users.noreply.github.com>
2024-03-27 18:20:40 +0100

    Ported changes from train to infer job script

--- jobs/wseg_infer.sh
+++ jobs/wseg_infer.sh
@@ -1,37 +1,64 @@
 #!/bin/bash
 
 # SLURM directives
-#SBATCH --gres=gpu:rtx5000:1
+#SBATCH --gres=gpu:v100:1
 #SBATCH --mem 128G
-#SBATCH -c 32
+#SBATCH -c 24
 #SBATCH -p gpu
 #SBATCH -t 2-00:00:00
 #SBATCH -o /usr/users/bhenne/projects/whisperseg/slurm_files/job-%J.out
 
-# Define paths
-base="/usr/users/bhenne/projects/whisperseg"
-code="$base"
-script="infer.py"
-data="$base/data/lemur_snippet_1/train"
-mtype="final_checkpoint_ct2" # final_checkpoint or final_checkpoint_ct2 for faster inference
-model="$base/model/whisperseg-lemur-finetuned-snip1/$mtype"
-output="$base/results"
-work="/local/eckerlab/wseg_data"
+# Definitions
+base_dir="/usr/users/bhenne/projects/whisperseg"
 
-# Prepare modules and conda environment for compute node
+code_dir="$base_dir"
+script1="infer.py"
+script2="evaluate.py"
+data_dir="$base_dir/data/inference"
+model_dir="$base_dir/model/<model_name>/final_checkpoint_ct2"
+output_dir="$base_dir/results"
+
+work_dir="/local/eckerlab/wseg_data"
+job_dir="$work_dir/$(date +"%Y%m%d_%H%M%S")_${SLURM_JOB_ID}_${script1%.*}"
+
+# Prevents excessive GPU memory reservation by Torch; enables batch sizes > 1 on v100s
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+
+# Function executes: on script exit, on error, on manual termination with ctrl-c
+cleanup() {
+    if [ -z "$cleanup_done" ]; then # otherwise cleanup runs twice for SIGINT or ERR
+        cleanup_done=true
+        echo "[JOB] Cleaning up..."
+        # Clean up: remove data, "<time>_<id>_<job>/" directory and parent working directory, if empty
+        rm -rf "$job_dir"
+        if [ -z "$(ls -A "${job_dir%/*}")" ]; then
+            rmdir "${job_dir%/*}"
+        fi
+        unset PYTORCH_CUDA_ALLOC_CONF
+    fi
+    exit $1
+}
+
+# Trap SIGINT signal (Ctrl+C), ERR signal (error), and script termination
+trap 'cleanup 1' SIGINT
+trap 'cleanup 1' ERR
+trap 'cleanup 0' EXIT
+
+# Prepare compute node environment
+echo "[JOB] Preparing environment..."
 module load anaconda3
 source activate wseg
 
 # Create temporary job directory and copy data
-destination="$work/$(date +"%Y%m%d_%H%M%S")_${script%.*}"
-mkdir -p "$destination"
-cp -r "$data"/* "$destination"
+echo "[JOB] Moving data to cluster..."
+mkdir -p "$job_dir"/data
+cp -r "$data_dir"/* "$job_dir/data"
 
-# Run script || true, ensuring cleanup even on failure
-python "$code/$script" --data_dir "$destination" --model_path "$model" --output_dir "$output" || true
+# Pre-training, usually on multispecies wseg model
+echo "[JOB] Computing inference..."
+python "$code_dir/$script" \
+    --data_dir "$job_dir/data" \
+    --model_path "$model_dir" \
+    --output_dir "$output_dir"
 
-# Clean up: remove data, "<time>_<job>/" directory and parent directory, if empty
-rm -rf "$destination"
-if [ -z "$(ls -A "${destination%/*}")" ]; then
-    rmdir "${destination%/*}"
-fi
+# Clean up (already handled by trap)
