e6790229b3f6e1cd125eb0cd4f1f8f34e578edb9
Benjamin Henne <13474968+benjaminhenne@users.noreply.github.com>
2024-03-27 17:03:34 +0100

    Improved training job script

--- jobs/wseg_train.sh
+++ jobs/wseg_train.sh
@@ -1,74 +1,98 @@
 #!/bin/bash
 
 # SLURM directives
-#SBATCH --gres=gpu:rtx5000:2
+#SBATCH --gres=gpu:v100:8
 #SBATCH --mem 128G
-#SBATCH -c 32
+#SBATCH -c 24
 #SBATCH -p gpu
 #SBATCH -t 2-00:00:00
 #SBATCH -o /usr/users/bhenne/projects/whisperseg/slurm_files/job-%J.out
 
-# Define paths
-base="/usr/users/bhenne/projects/whisperseg"
-code="$base"
+# Definitions
+base_dir="/usr/users/bhenne/projects/whisperseg"
+
+code_dir="$base_dir"
 script1="train.py"
 script2="evaluate.py"
-data_pre="$base/data/lemur_snippet_1/train"
-data_fine="$base/data/lemur_snippet_1/train"
-model_in="nccratliri/whisperseg-base-animal-vad"
-model_out="$base/model/$(date +"%Y%m%d_%H%M%S")_${SLURM_JOB_ID}_wseg-lemur"
-work="/local/eckerlab/wseg_data"
-output="$base/results"
-output_identifier="cfg1"
+data_dir_pre="$base_dir/data/lemur/train-pre"
+data_dir_fine="$base_dir/data/lemur/train-fine"
+model_dir_in="nccratliri/whisperseg-animal-vad"
+model_dir_out="$base_dir/model/$(date +"%Y%m%d_%H%M%S")_j${SLURM_JOB_ID}_wseg-large"
+output_dir="$base_dir/results"
+output_identifier=""
+
+work_dir="/local/eckerlab/wseg_data"
+job_dir="$work_dir/$(date +"%Y%m%d_%H%M%S")_${SLURM_JOB_ID}_${script1%.*}"
 
 epochs=6
-batch_size=4
+batch_size=1
+
+# Prevents excessive GPU memory reservation by Torch; enables batch sizes > 1 on v100s
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+
+# Function executes: on script exit, on error, on manual termination with ctrl-c
+cleanup() {
+    if [ -z "$cleanup_done" ]; then # otherwise cleanup runs twice for SIGINT or ERR
+        cleanup_done=true
+        echo "[JOB] Cleaning up..."
+        # Clean up: remove data, "<time>_<id>_<job>/" directory and parent working directory, if empty
+        rm -rf "$job_dir"
+        if [ -z "$(ls -A "${job_dir%/*}")" ]; then
+            rmdir "${job_dir%/*}"
+        fi
+        unset PYTORCH_CUDA_ALLOC_CONF
+    fi
+    exit $1
+}
+
+# Trap SIGINT signal (Ctrl+C), ERR signal (error), and script termination
+trap 'cleanup 1' SIGINT
+trap 'cleanup 1' ERR
+trap 'cleanup 0' EXIT
 
 # Prepare compute node environment
+echo "[JOB] Preparing environment..."
 gpus=$(echo $CUDA_VISIBLE_DEVICES | tr ',' ' ')
 module load anaconda3
 source activate wseg
 
 # Create temporary job directory and copy data
-destination="$work/$(date +"%Y%m%d_%H%M%S")_${SLURM_JOB_ID}_${script1%.*}"
-mkdir -p "$destination"/data/{pretrain,finetune}
-mkdir -p "$destination"/{pretrain_ckpt,finetune_ckpt}
-cp -r "$data_pre"/* "$destination/data/pretrain/"
-cp -r "$data_fine"/* "$destination/data/finetune/"
+echo "[JOB] Moving data to cluster..."
+mkdir -p "$job_dir"/data/{pretrain,finetune}
+mkdir -p "$job_dir"/{pretrain_ckpt,finetune_ckpt}
+cp -r "$data_dir_pre"/* "$job_dir/data/pretrain"
+cp -r "$data_dir_fine"/* "$job_dir/data/finetune"
 
-# Run scripts || true, ensuring cleanup even on failure
 # Pre-training, usually on multispecies wseg model
-python "$code/$script1" \
-    --initial_model_path "$model_in" \
-    --train_dataset_folder "$destination/data/pretrain/" \
-    --model_folder "$destination/pretrain_ckpt/" \
+echo "[JOB] Pretraining..."
+python "$code_dir/$script1" \
+    --initial_model_path "$model_dir_in" \
+    --train_dataset_folder "$job_dir/data/pretrain" \
+    --model_folder "$job_dir/pretrain_ckpt" \
     --gpu_list $gpus \
     --max_num_epochs $epochs \
-    --batch_size $batch_size \
-    || true
+    --batch_size $batch_size
 
 # Fine-tuning
-python "$code/$script1" \
-    --initial_model_path "$destination/pretrain_ckpt/final_checkpoint" \
-    --train_dataset_folder "$destination/data/finetune" \
-    --model_folder "$destination/finetune_ckpt/" \
+echo "[JOB] Finetuning..."
+python "$code_dir/$script1" \
+    --initial_model_path "$job_dir/pretrain_ckpt/final_checkpoint" \
+    --train_dataset_folder "$job_dir/data/finetune" \
+    --model_folder "$job_dir/finetune_ckpt" \
     --gpu_list $gpus \
     --max_num_epochs $epochs \
-    --batch_size $batch_size \
-    || true
+    --batch_size $batch_size
 
-python "$code/$script2" \
-    -d "$destination/data/finetune" \
-    -m "$destination/finetune_ckpt/final_checkpoint_ct2" \
-    -o "$output" \
-    -i "$output_identifier" \
-    || true
+# Evaluation
+echo "[JOB] Evaluating..."
+python "$code_dir/$script2" \
+    -d "$job_dir/data/finetune" \
+    -m "$job_dir/finetune_ckpt/final_checkpoint_ct2" \
+    -o "$output_dir" \
+    -i "$output_identifier"
 
-# Move finished model to target destination
-mv "$destination/finetune_ckpt" "$model_out"
+# Move finished model to target job_dir
+echo "[JOB] Moving trained model..."
+mv "$job_dir/finetune_ckpt" "$model_dir_out"
 
-# Clean up: remove data, "<time>_<job>/" directory and parent directory, if empty
-rm -rf "$destination"
-if [ -z "$(ls -A "${destination%/*}")" ]; then
-    rmdir "${destination%/*}"
-fi
+# Clean up (already handled by trap)
