a4003699e1f113ce81717d7c0caa9791553ba12a
Benjamin Henne <13474968+benjaminhenne@users.noreply.github.com>
2024-05-05 14:25:03 +0200

    Added functions to calculate annotation metadata: clip duration, hop length, sampling rate, spec_time_step

--- util/common.py
+++ util/common.py
@@ -41,3 +41,41 @@ def get_flex_file_iterator(file_path: str, rglob_str: str = "*.txt") -> Generato
             return p.rglob(rglob_str)
         else:
             raise ValueError(f"Invalid file path: {file_path}")
+
+def compute_annotation_metadata(duration: float = None, sampling_rate: int = None, l_hop: float = None) -> float:
+    """
+    Given duration and sampling_rate: Compute the hop length of the spectrogram.
+    Given duration and hop length: Compute the sampling rate of the audio file.
+    Given sampling rate and hop length: Compute the audio clip duration.
+    For specifics, refer to: https://doi.org/10.1101/2023.09.30.560270
+
+    Args:
+        duration (float, optional): Audio clip duration in seconds in Hz (not kHz). Defaults to None.
+        sampling_rate (int, optional): Sampling rate of the audio file. Defaults to None.
+        l_hop (float, optional): Spectrogram hop length. Defaults to None.
+
+    Returns:
+        float: The computed value
+    """
+    if duration and sampling_rate and not l_hop:
+        return (duration * sampling_rate) / 1000
+    elif duration and l_hop and not sampling_rate:
+        return int((l_hop * 1000) / duration)
+    elif sampling_rate and l_hop and not duration:
+        return (l_hop * 1000) / sampling_rate
+    else:
+        raise ValueError("Invalid input. Provide two of the three arguments: duration, sampling_rate, l_hop")
+
+def compute_spec_time_step(sampling_rate: int, l_hop: float) -> float:
+    """
+    Compute the spect_time_step parameter given the sampling rate and hop length.
+    For specifics, refer to: https://github.com/bhnn/whisperseg/blob/master/docs/DatasetProcessing.md
+
+    Args:
+        sampling_rate (int): Sampling rate of the audio file in Hz (not kHz)
+        l_hop (float): Spectrogram hop length
+
+    Returns:
+        float: The computed value
+    """
+    return l_hop / sampling_rate
