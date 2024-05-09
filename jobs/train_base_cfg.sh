71290a973c97949efeda89fbc37cfa5b091d4d40
Benjamin Henne <13474968+benjaminhenne@users.noreply.github.com>
2024-05-09 23:22:11 +0200

    Added early stopping, patience, wandb_dir, val_ratio and conditional moving of final results to base_cfg and large_cfg jobs

--- jobs/train_base_cfg.sh
+++ jobs/train_base_cfg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # SLURM directives
-#SBATCH --gres=gpu:rtx5000:4
+#SBATCH --gres=gpu:rtx5000:1
 #SBATCH --mem 128G
 #SBATCH -c 16
 #SBATCH -p gpu
@@ -28,15 +28,17 @@ data_dir_test="$base_dir/data/lemur_${cfg}/test"
 model_dir_in="nccratliri/whisperseg-base-animal-vad"
 model_dir_out="$base_dir/model/$(date +"%Y%m%d_%H%M%S")_j${SLURM_JOB_ID}_wseg-base-cfg-$cfg"
 output_dir="$base_dir/results"
-output_identifier="base_cfg${cfg}"
+output_identifier="base_cfg${cfg}_j${SLURM_JOB_ID}"
 
 work_dir="/local/eckerlab/wseg_data"
 job_dir="$work_dir/$(date +"%Y%m%d_%H%M%S")_${SLURM_JOB_ID}_${script1%.*}"
-wandb_dir="/local/jobs/$SLURM_JOB_ID"
+wandb_dir=$job_dir
 
-epochs=6
+epochs=100
 batch_size=4
-wandb_notes="cfg${cfg}, 4x rtx5000, bs${batch_size}, ep${epochs}"
+patience=10
+val_ratio=0.2
+wandb_notes="cfg${cfg}, rtx5000:1, bs${batch_size}, ep${epochs}, vratio${val_ratio}, pat${patience}"
 
 # Prevents excessive GPU memory reservation by Torch; enables batch sizes > 1 on v100s
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
@@ -68,7 +70,7 @@ source activate wseg
 # Create temporary job directory and copy data
 echo "[JOB] Moving data to cluster..."
 mkdir -p "$job_dir"/data/{pretrain,finetune,test}
-mkdir -p "$job_dir"/{pretrain_ckpt,finetune_ckpt}
+mkdir -p "$job_dir"/{pretrain_ckpt,finetune_ckpt,wandb}
 cp -r "$data_dir_pre"/* "$job_dir/data/pretrain"
 cp -r "$data_dir_fine"/* "$job_dir/data/finetune"
 cp -r "$data_dir_test"/* "$job_dir/data/test"
@@ -84,7 +86,11 @@ python "$code_dir/$script1" \
     --batch_size $batch_size \
     --run_name $SLURM_JOB_ID-0 \
     --run_notes "pretrain, ${wandb_notes}" \
-    --wandb_dir "$wandb_dir"
+    --wandb_dir "$wandb_dir" \
+    --validate_per_epoch 1 \
+    --val_ratio $val_ratio \
+    --save_per_epoch 1 \
+    --patience $patience
 
 # Fine-tuning
 echo "[JOB] Finetuning..."
@@ -97,7 +103,11 @@ python "$code_dir/$script1" \
     --batch_size $batch_size \
     --run_name $SLURM_JOB_ID-1 \
     --run_notes "finetune, ${wandb_notes}" \
-    --wandb_dir "$wandb_dir"
+    --wandb_dir "$wandb_dir" \
+    --validate_per_epoch 1 \
+    --val_ratio $val_ratio \
+    --save_per_epoch 1 \
+    --patience $patience
 
 # Evaluation
 echo "[JOB] Evaluating..."
@@ -108,7 +118,9 @@ python "$code_dir/$script2" \
     -i "$output_identifier"
 
 # Move finished model to target job_dir
-echo "[JOB] Moving trained model..."
-mv "$job_dir/finetune_ckpt" "$model_dir_out"
+if [ -n "$(ls -A "$job_dir/finetune_ckpt")" ]; then
+    echo "[JOB] Moving trained model..."
+    mv "$job_dir/finetune_ckpt" "$model_dir_out"
+fi
 
 # Clean up (already handled by trap)
