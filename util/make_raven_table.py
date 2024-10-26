ac77083331b5e7850241d5dda74da3e2f9c16404
Benjamin Henne <13474968+benjaminhenne@users.noreply.github.com>
2024-10-26 19:02:06 +0200

    Added handling for 0-prediction files (for aug150 stuff)

--- util/make_raven_table.py
+++ util/make_raven_table.py
@@ -7,21 +7,24 @@ from pathlib import Path
 import yaml
 
 
-def make_raven_tables(file_path: str, config_path: str, output_path: str = ""):
+def make_raven_tables(file_path: str, extension: str, config_path: str, output_path: str = ""):
     """Converts WhisperSeg inference results into Raven selection tables
 
     Args:
         file_path (str): Path to .jsonr inference files.
+        extension (str): Extention of the inference files: either jsonr or json, defaults to jsonr
         config_path (str): Path to the config file detailing all annotation classes.
         output_path (str, optional): Path to an output directory. Defaults to the input directory.
     """
     with open(config_path, 'r') as f:
         classes = yaml.safe_load(f)
         classes.pop('misc', None) # to remove labels such as 'fluff' and 'help'
-    for p in Path(file_path).rglob("*.jsonr"):
+    for p in Path(file_path).rglob(f"*.{extension}"):
         logging.info(f"Found: {p}")
         with open(p, "r") as f:
             res_file = json.load(f)
+        if len(res_file["onset"]) < 1:
+            continue
         if len(res_file["onset"]) == len(res_file["offset"]) == len(res_file["cluster"]):
             id = 1
             out = ["Selection	View	Channel	Begin Time (s)	End Time (s)	Low Freq (Hz)	High Freq (Hz)	Delta Time (s)	Delta Freq (Hz)	Avg Power Density (dB FS/Hz)	Annotation"]
@@ -44,7 +47,8 @@ if __name__ == "__main__":
     logging.getLogger().setLevel(logging.INFO)
 
     parser = argparse.ArgumentParser(description="Converts WhisperSeg inference results into Raven selection tables")
-    parser.add_argument("-p", "--file_path", type=str, help="Path to .jsonr inference files", required=True)
+    parser.add_argument("-p", "--file_path", type=str, help="Path to inference files", required=True)
+    parser.add_argument("-e", "--extension", type=str, help="Extention of the inference files: either jsonr or json", default="jsonr")
     parser.add_argument("-c", "--config_path", type=str, help="Path to the config file detailing all annotation classes", default='./config/classes.yaml')
     parser.add_argument("-o", "--output_path", type=str, help="Path to the output .txt  selection table files. Defaults to the input directory.", default=None)
     args = parser.parse_args()
